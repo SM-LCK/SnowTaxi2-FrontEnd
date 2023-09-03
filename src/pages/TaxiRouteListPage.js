b9999e8d810a67ea3ecc83f19bf809275d3ab608
lotuxsoo <https://github.com/lotuxsoo>
2023-09-04 01:01:03 +0900

    0903

---
+++ src/pages/TaxiRouteListPage.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+const TaxiRouteListPage = () => {
+  return <h3>taxiroute</h3>;
+};
+
+export default TaxiRouteListPage;

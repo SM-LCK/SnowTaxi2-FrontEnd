96ebe72b482f04ea6455f4affa760ff242f8a87f
kelly1422 <kelly1422@naver.com>
2023-10-03 00:36:30 +0900

    [FEAT]: 채팅

--- src/pages/ChattingPage.js
+++ src/pages/ChattingPage.js
@@ -4,6 +4,7 @@ import ReactRoundedImage from "react-rounded-image";
 import * as StompJs from "@stomp/stompjs";
 import { Button } from "react-bootstrap";
 import profile1 from "../assets/profile1.png";
+import axios from "axios";
 
 
 // 호출할 떄 <ChattingPage id={localStorage.getItem("@potId")} />
@@ -14,7 +15,7 @@ const ChattingPage = props => {
   let participaitngPotId = localStorage.getItem("@potId")
 
   useEffect(() => {
-    console.log(me)
+    getChatsAxios();
     connect();
     return () => disConnect();
     console.log("here", participaitngPotId)
@@ -24,84 +25,28 @@ const ChattingPage = props => {
 
   let [client, changeClient] = useState(null);
   const [chat, setChat] = useState(""); // 입력된 chat을 받을 변수
-  const [chatList, setChatList] = useState([{
-    "sender": "d",
-    "roomId": 1,
-    "content": "hellosdfbjshdfbhjsdbfsdbfhsdbhjfbsdjhfbsdjhfcdjhbfgcnjhdfjngcjhdfbngjhkdngsbfchjrnbfjhnbxdruhigxnfersuiy",
-    "type": "TALK",
-    "time": "12:30"
-}]); // 채팅 기록
+  const [chatList, setChatList] = useState([]); // 채팅 기록
 
-  // 내가 보낸 메시지, 받은 메시지에 각각의 스타일을 지정해 주기 위함
-  const msgBox = chatList.map((item, idx) => {
-    if (item.sender == me) {
-      return (
-        <div style={{display:"flex", justifyContent:"flex-end", padding:"3px"}}>
-          <div style={{display:"flex", justifyContent:"flex-end", width:wWidth/3}}>
-            <div>{item.time}</div>
-            <div style={{padding:"10px"}}>
-              <div style={{
-                      width: "max-content",
-                      maxWidth: "380px",
-                      wordBreak: "break-all",
-                      backgroundColor: "#84BBFF", 
-                      fontSize:"20px", 
-                      marginTop:"10px",
-                      borderTopLeftRadius: "18px",
-                      borderBottomRightRadius: "18px",
-                      borderBottomLeftRadius: "18px",
-                      marginRight:"7px",
-                      padding:"18px"
-                    }}
-              >
-                {item.content}
-              </div>
-            </div>
-            <ReactRoundedImage
-                image = {profile1}
-                roundedColor="#5E5E5E"
-                roundedSize="5"
-                imageWidth="60"
-                imageHeight="60"
-              />
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div style={{display:"flex", justifyContent:"flex-start", padding:"10px"}}>
-          <div style={{display:"flex", justifyContent:"flex-start", width:wWidth/3, backgroundColor:"red"}}>
-            <ReactRoundedImage
-                image = {profile1}
-                roundedColor="#5E5E5E"
-                roundedSize="5"
-                imageWidth="60"
-                imageHeight="60"
-            />
-            <div style={{padding:"5px"}}>
-              <div style={{
-                      width: "max-content",
-                      maxWidth: "380px",
-                      wordBreak: "break-all",
-                      backgroundColor: "#E0E0E0", 
-                      fontSize:"20px", 
-                      marginTop:"10px",
-                      borderTopRightRadius: "18px",
-                      borderBottomRightRadius: "18px",
-                      borderBottomLeftRadius: "18px",
-                      marginLeft:"7px",
-                      padding: "18px"
-                    }}
-              >
-                {item.content}
-              </div>
-            </div>
-            <div>{item.time}</div>
-          </div>
-        </div>
-      );
+  const getChatsAxios = async () => {
+    try {
+      axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}/chatroom`,
+        params: { roomId: participaitngPotId },
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("@token")}`,
+        },
+      })
+      .then((response) => {
+        setChatList(response.data.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    } catch (error) {
+      console.log(error);
     }
-  });
+  };
 
   const connect = () => {
     // 소켓 연결
@@ -155,8 +100,7 @@ const ChattingPage = props => {
           sender: me,
           roomId: participaitngPotId,
           content: chat,
-          type: "TALK",
-          time: "12:30"
+          type: "TALK"
       }),
     });
 
@@ -166,6 +110,82 @@ const ChattingPage = props => {
   const onChangeChat = (e) => {
     setChat(e.target.value);
   };
+
+  // 내가 보낸 메시지, 받은 메시지에 각각의 스타일을 지정해 주기 위함
+  const msgBox = chatList.map((item, idx) => {
+    if (item.type != "TALK") {
+      // 들어가고 나가는 메세지
+    } else {
+      if (item.sender == me) {
+        return (
+          <div style={{display:"flex", justifyContent:"flex-end", padding:"3px"}}>
+            <div style={{display:"flex", justifyContent:"flex-end", width:wWidth/3}}>
+              <div>{item.time}</div>
+              <div style={{padding:"10px"}}>
+                <div style={{
+                        width: "max-content",
+                        maxWidth: "380px",
+                        wordBreak: "break-all",
+                        backgroundColor: "#84BBFF", 
+                        fontSize:"20px", 
+                        marginTop:"10px",
+                        borderTopLeftRadius: "18px",
+                        borderBottomRightRadius: "18px",
+                        borderBottomLeftRadius: "18px",
+                        marginRight:"7px",
+                        padding:"18px"
+                      }}
+                >
+                  {item.content}
+                </div>
+              </div>
+              <ReactRoundedImage
+                  image = {profile1}
+                  roundedColor="#5E5E5E"
+                  roundedSize="5"
+                  imageWidth="60"
+                  imageHeight="60"
+                />
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div style={{display:"flex", justifyContent:"flex-start", padding:"10px"}}>
+            <div style={{display:"flex", justifyContent:"flex-start", width:wWidth/3, backgroundColor:"red"}}>
+              <ReactRoundedImage
+                  image = {profile1}
+                  roundedColor="#5E5E5E"
+                  roundedSize="5"
+                  imageWidth="60"
+                  imageHeight="60"
+              />
+              <div style={{padding:"5px"}}>
+                <div style={{
+                        width: "max-content",
+                        maxWidth: "380px",
+                        wordBreak: "break-all",
+                        backgroundColor: "#E0E0E0", 
+                        fontSize:"20px", 
+                        marginTop:"10px",
+                        borderTopRightRadius: "18px",
+                        borderBottomRightRadius: "18px",
+                        borderBottomLeftRadius: "18px",
+                        marginLeft:"7px",
+                        padding: "18px"
+                      }}
+                >
+                  {item.content}
+                </div>
+              </div>
+              <div>{item.time}</div>
+            </div>
+          </div>
+        );
+      }
+    }
+
+  });
 
   return (
     <div style={{justifyContent: "center", display: "flex"}}>
